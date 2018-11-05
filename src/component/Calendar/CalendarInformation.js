873a8698c0026d7a6835f6a7576ee5f41d34fb8e
JohnWAnderson <John.Anderson45@yahoo.com>
2018-11-05 13:10:42 -0800

    pre merge commit

--- src/component/Calendar/CalendarInformation.js
+++ src/component/Calendar/CalendarInformation.js
@@ -1,28 +1,24 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { Container, Row, Col } from 'reactstrap';
 
-// const PageInfoRow = styled(Row)`
-//     color: black;
-//     text-decoration: none;
-//     font-family: Georgia;
-//     font-size: 20px;
-//     position: relative;
-//     vertical-align: middle;
-//     text-align:center;
-// `
+const PageInfoRow = styled(Row)`
+    color: black;
+    text-decoration: none;
+    font-family: Georgia;
+    font-size: 20px;
+    position: relative;
+    vertical-align: middle;
+    text-align:center;
+`
 
 export default (props) =>{
     return(
-        <div>  
-            {props.items > 0 ? 
-            <div>This month contains {props.items} items which totals to: ${(props.cost/100).toFixed(2)}</div>:
-            <div>Page month no items</div>}
-        </div>
+        <Container>
+        {props.items > 0 ? 
+            <div><PageInfoRow><Col><h1>Month contains {props.items} items</h1></Col></PageInfoRow> <PageInfoRow><Col><h1>Totaling ${props.cost/100}</h1></Col></PageInfoRow></div>:
+            <PageInfoRow>Page contains no items</PageInfoRow>}
+        </Container>
     );
 }
-
-// {props.items > 0 ? 
-//     <div></><PageInfoRow><Col><h1>This month contains {props.items} items</h1></Col></PageInfoRow> <PageInfoRow><Col><h1>Totaling ${props.Sum/100}</h1></Col></PageInfoRow></div>:
-//     <PageInfoRow>Page contains no items</PageInfoRow>}
-// </Container>
