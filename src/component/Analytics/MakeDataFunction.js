8beda32dce98180a926e8717195b591d82489985
JohnWAnderson <John.Anderson45@yahoo.com>
2018-11-16 10:40:12 -0800

    updated colors

--- src/component/Analytics/MakeDataFunction.js
+++ src/component/Analytics/MakeDataFunction.js
@@ -2,15 +2,27 @@ export default (items)=>{
     let labels =[];
     let backgroundColor = [];
     let data = [];
+    let i =0;
     for (const [k, v] of items) {
         labels = [...labels, k];
-        backgroundColor = [...backgroundColor, RandomColor()];
+        backgroundColor = [...backgroundColor, RandomColor(i)];
         data = [...data , (v/100).toFixed(2)];
+        i = i+1;
     }    
     return {labels: labels, datasets:[{backgroundColor: backgroundColor, data:data}]}  
 }
 
-const RandomColor = () =>{
-    var max = 0xffffff;
+const colors = ['#3cb44b', '#e6194b', '#ffe119', '#4363d8', '#f58231', 
+'#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', 
+'#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080']
+
+const RandomColor = (Spot) =>{
+    if(Spot < colors.length){
+        return colors[Spot];
+    }
+    else{
+    let max = 0xffffff;
     return '#' + Math.round( Math.random() * max ).toString( 16 );
+    }
 }
+
